49b65becb352a085d9db6f0aaae2224456538a38
Jamiu Abidemi Abdulkareem <106557387+jamiuabdulkareem@users.noreply.github.com>
2023-08-24 19:37:57 +0100

    upload

--- src/components/WithdrawFund.jsx
+++ src/components/WithdrawFund.jsx
@@ -1,9 +1,21 @@
+import Button from "./Button";
 import Popup from "./Popup";
+import styles from "./WithdrawFunds.module.css";
 
 function WithdrawFund() {
-  return <>
-  <Popup/>
-  </>;
+  return (
+    <>
+      <Popup>
+        <h3 className={styles.h3}>Withdraw Funds</h3>
+        <form className={styles.form}>
+          <input type="text" placeholder="Enter full name" />
+          <input type="text" placeholder="Enter bank name" />
+          <input type="tel" placeholder="Enter account number" />
+          <Button>Add Bank</Button>
+        </form>
+      </Popup>
+    </>
+  );
 }
 
 export default WithdrawFund;
