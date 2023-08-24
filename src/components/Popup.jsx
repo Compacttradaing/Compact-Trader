78a979d1dc2ed60482ce9d7cd0c5ad5c7bd2c1fb
Jamiu Abidemi Abdulkareem <106557387+jamiuabdulkareem@users.noreply.github.com>
2023-08-24 18:32:47 +0100

    update

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -1,13 +1,19 @@
-import Table from "./Table";
 import styles from "./Popup.module.css";
 import { IoMdClose } from "react-icons/io";
+import { useNavigate } from "react-router-dom";
 
-function Popup() {
+function Popup({ children }) {
+  const navigate = useNavigate();
+
+  function handleHidden() {
+    navigate(-1);
+  }
+
   return (
     <div className={styles.popup}>
       <div className={styles.popupContent}>
-        <IoMdClose className={styles.closeIcon} />
-        <p>Hello</p>
+        <IoMdClose className={styles.closeIcon} onClick={handleHidden} />
+        {children}
       </div>
     </div>
   );
