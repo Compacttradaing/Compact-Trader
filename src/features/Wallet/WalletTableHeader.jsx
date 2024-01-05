3d1507fc21dd1a05abe1b35a9bbf1ab96cc2a957
Jamiu Abidemi Abdulkareem <106557387+jamiuabdulkareem@users.noreply.github.com>
2024-01-06 00:33:53 +0100

    update

--- src/features/Wallet/WalletTableHeader.jsx
+++ src/features/Wallet/WalletTableHeader.jsx
@@ -1,6 +1,6 @@
 function WalletTableHeader() {
   return (
-    <header className="grid grid-cols-4 md:grid-cols-4 uppercase font-semibold justify-center items-center border-b text-sm md:text-lg text-center bg-slate-100 border-slate-300 gap-1 md:gap-2.5 px-2 py-2.5">
+    <header className="grid grid-cols-4 md:grid-cols-4 font-medium capitalize md:uppercase sm:font-semibold justify-center items-center border-b text-sm md:text-lg text-center bg-slate-100 border-slate-300 gap-1 md:gap-2.5 px-2 py-2.5">
       <h2>Bank</h2>
       <h2>Name</h2>
       <h2>Account number</h2>
--- src/features/Wallet/WalletTableRow.jsx
+++ src/features/Wallet/WalletTableRow.jsx
@@ -1,3 +1,4 @@
+import { IoTrash } from "react-icons/io5";
 import TableBtn from "../../ui/TableBtn";
 
 function WalletTableRow() {
@@ -5,8 +6,13 @@ function WalletTableRow() {
     <div className="grid grid-cols-4 md:grid-cols-4 text-sm items-center justify-center text-center gap-x-2.5 px-1.5 py-2.5 last:border-b last:border-slate-300">
       <h2>Access</h2>
       <h2>John olasebikan</h2>
-      <h2>432887383733</h2>
-      <TableBtn>Edit</TableBtn>
+      <h2>4328873837</h2>
+      <div className="flex items-center gap-2">
+        <TableBtn>Edit</TableBtn>
+        <button>
+          <IoTrash className="text-red-500 text-xl" />
+        </button>
+      </div>
     </div>
   );
 }
