53c17225a2aeb0a247d51f0f72b952ca71f590bb
Jamiu Abidemi Abdulkareem <106557387+jamiuabdulkareem@users.noreply.github.com>
2024-01-05 03:35:33 +0100

    update

--- src/features/transaction/TransactionHeader.jsx
+++ src/features/transaction/TransactionHeader.jsx
@@ -1,7 +1,7 @@
 function TransactionHeader() {
   return (
-    <header className="grid grid-cols-6 items-center border-b text-center bg-slate-100 border-slate-300 gap-2 px-2 py-2.5">
-      <h2>TradeID</h2>
+    <header className="grid grid-cols-6 uppercase font-semibold items-center border-b text-center bg-slate-100 border-slate-300 gap-2 px-2 py-2.5">
+      <h2 className="capitalize hidden md:block">TradeID</h2>
       <h2>Type</h2>
       <h2>Time</h2>
       <h2>Amount</h2>
---
+++ src/features/transaction/TransactionTableRow.jsx
@@ -0,0 +1,14 @@
+function TransactionTableRow() {
+  return (
+    <div className="grid grid-cols-6 text-sm items-center text-center gap-x-2.5 px-1.5 py-2.5 last:border-b last:border-slate-300">
+      <h2 className="hidden md:block">TradeID</h2>
+      <h2>Type</h2>
+      <h2>Time</h2>
+      <h2>Amount</h2>
+      <h2>Status</h2>
+      {/* <h2>TradeID</h2> */}
+    </div>
+  );
+}
+
+export default TransactionTableRow;
