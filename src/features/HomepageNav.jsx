5e245f57383f988ba7bb43339877fd1625f10c69
Jamiu Abidemi Abdulkareem <106557387+jamiuabdulkareem@users.noreply.github.com>
2024-01-14 03:02:08 +0100

    Update HomepageNav.jsx

--- src/features/HomepageNav.jsx
+++ src/features/HomepageNav.jsx
@@ -1,19 +1,21 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
 function HomepageNav() {
-   return (
-      
-         <nav className="flex items-center justify-between mt-5">
-            <h1 className="text-xl font-bold text-slate-900">Compact</h1>
-            <nav>
-               <ul className="flex items-center justify-center gap-2">
-                  <li className="text-sm text-slate-500 font-medium"><Link to="/login">Sign in</Link></li>
-                  <li className="text-sm font-medium text-slate-500"><Link to="/register">Get started</Link></li>
-               </ul>
-            </nav>
-         </nav>
-      
-   )
+  return (
+    <nav className="flex items-center justify-between mt-5">
+      <span className="text-xl font-bold text-slate-900">Compact</span>
+      <nav>
+        <ul className="flex items-center justify-center gap-2">
+          <li className="text-sm text-slate-500 font-medium">
+            <Link to="/login">Sign in</Link>
+          </li>
+          <li className="text-sm font-medium text-slate-500">
+            <Link to="/register">Get started</Link>
+          </li>
+        </ul>
+      </nav>
+    </nav>
+  );
 }
 
-export default HomepageNav
+export default HomepageNav;
