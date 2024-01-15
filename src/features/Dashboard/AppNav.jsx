19796c63e3e31922610ceb77f7c3441197ad4790
Jamiu Abidemi Abdulkareem <106557387+jamiuabdulkareem@users.noreply.github.com>
2024-01-16 00:25:39 +0100

    Update AppNav.jsx

--- src/features/Dashboard/AppNav.jsx
+++ src/features/Dashboard/AppNav.jsx
@@ -9,34 +9,34 @@ import { NavLink } from "react-router-dom";
 
 function AppNav() {
   return (
-    <nav className="fixed left-0 right-0 px-2 sm:px-20 py-3 bottom-0 bg-indigo-100 drop-shadow-md ">
+    <nav className="fixed left-0 right-0 px-2 sm:px-20 sm:py-3 py-1 bottom-0 bg-indigo-100 drop-shadow-md ">
       <ul className="flex items-center justify-between">
         <NavLink
           to=""
           className="grid place-items-center transition-all duration-300 active:text-indigo-950 visited:text-indigo-950 hover:text-indigo-900 text-slate-900"
         >
-          <IoHomeOutline className="text-2xl" />
+          <IoHomeOutline className="text-xl sm:text-2xl" />
           <span className="text-sm font-medium">Home</span>
         </NavLink>
         <NavLink
           to="wallet"
           className="grid place-items-center transition-all duration-300 visited:text-indigo-950 hover:text-indigo-900 text-slate-900"
         >
-          <IoWalletOutline className="text-2xl" />
+          <IoWalletOutline className="text-xl sm:text-2xl" />
           <span className="text-sm font-medium">Wallet</span>
         </NavLink>
         <NavLink
           to="transaction"
           className="grid place-items-center transition-all duration-300 active:text-indigo-900 visited:text-indigo-900 hover:text-indigo-900 text-slate-900"
         >
-          <IoPieChartOutline className="text-2xl" />
+          <IoPieChartOutline className="text-xl sm:text-2xl" />
           <span className="text-sm font-medium">Transaction</span>
         </NavLink>
         <NavLink
           to="profile"
           className="grid place-items-center transition-all duration-300 active:text-indigo-900 visited:text-indigo-900 hover:text-indigo-900 text-slate-900"
         >
-          <IoPersonOutline className="text-2xl" />
+          <IoPersonOutline className="text-xl sm:text-2xl" />
           <span className="text-sm font-medium">Profile</span>
         </NavLink>
       </ul>
