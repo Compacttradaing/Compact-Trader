a2b6a333c64a0b6ed523d6b79ab60c84dcbf71aa
Jamiu Abidemi Abdulkareem <106557387+jamiuabdulkareem@users.noreply.github.com>
2024-01-16 03:07:30 +0100

    update login

--- src/features/Dashboard/Header.jsx
+++ src/features/Dashboard/Header.jsx
@@ -1,10 +1,17 @@
 import { IoCloseOutline } from "react-icons/io5";
+import { useLogOut } from "../authentication/useLogOut";
 
 function Header() {
+  const { logout, isLoading } = useLogOut();
+
   return (
     <header className="flex items-center justify-between">
       <h1 className="font-semibold text-slate-900">Welcome, John</h1>
-      <button className="flex items-center justify-center gap-1">
+      <button
+        disabled={isLoading}
+        onClick={logout}
+        className="flex items-center justify-center gap-1"
+      >
         <span className="text-sm text-slate-900">Sign Out</span>{" "}
         <IoCloseOutline className="text-red-500 text-xl" />
       </button>
