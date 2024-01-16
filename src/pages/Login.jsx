690e8dc985fbddbe0e28a88ddd150318dee4a76a
Jamiu Abidemi Abdulkareem <106557387+jamiuabdulkareem@users.noreply.github.com>
2024-01-16 01:46:27 +0100

    update login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,38 +2,68 @@ import { Link } from "react-router-dom";
 import Button from "../ui/Button";
 import FormRow from "../ui/FormRow";
 import Input from "../ui/Input";
+import { useState } from "react";
+import { login } from "../services/apiAuth";
+import { useLogin } from "../features/authentication/useLogin";
 
 function Login() {
+  const [email, setEmail] = useState("jamiuabdulkareem08@gmail.com");
+  const [password, setPassword] = useState("90903");
+  const { login, isLoading } = useLogin();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!email || !password) return;
+    login({ email, password });
+  }
+
   return (
-    <>
-      <div className="bg-[url('./images/backgroundPatern.png')] bg-indigo-900 bg-blend-multiply h-screen w-screen flex justify-center gap-20 items-center px-5 sm:px-40">
-        <form className="bg-slate-50 py-7 px-7 w-full sm:w-96 drop-shadow-xl rounded-md">
-          <FormRow>
-            <Input type="email" placeholder="Email Address" />
-          </FormRow>
-          <FormRow>
-            <Input type="password" placeholder="Password" />
-          </FormRow>
+    <div className="bg-[url('./images/backgroundPatern.png')] bg-indigo-900 bg-blend-multiply h-screen w-screen flex justify-center gap-20 items-center px-5 sm:px-40">
+      <form
+        onSubmit={handleSubmit}
+        className="bg-slate-50 py-7 px-7 w-full sm:w-96 drop-shadow-xl rounded-md"
+      >
+        <FormRow>
+          <Input
+            type="email"
+            placeholder="Email Address"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            disabled={isLoading}
+            required
+          />
+        </FormRow>
+        <FormRow>
+          <Input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            disabled={isLoading}
+            required
+          />
+        </FormRow>
 
-          <Button type="secondary" onClick={() => alert("Login")}>
-            Sign in
-          </Button>
+        <Button disabled={isLoading} type="secondary">Sign in</Button>
 
-          <div>
-            <p className="text-sm text-slate-500 mt-4 text-center">
-              Create new account,{" "}
-              <Link
-                to="/register"
-                className="text-lg text-slate-900 hover:underline transition-all duration-300"
-              >
-                Register here
-              </Link>
-            </p>
-          </div>
-        </form>
-      </div>
-    </>
+        <div>
+          <p className="text-sm text-slate-500 mt-4 text-center">
+            Create new account,{" "}
+            <Link
+              to="/register"
+              className="text-lg text-slate-900 hover:underline transition-all duration-300"
+            >
+              Register here
+            </Link>
+          </p>
+        </div>
+      </form>
+    </div>
   );
 }
 
 export default Login;
+
+{
+  /*  */
+}
--- src/ui/Input.jsx
+++ src/ui/Input.jsx
@@ -1,5 +1,14 @@
-function Input({ type, placeholder }) {
-  return <input type={type} className="input" placeholder={placeholder} />;
+function Input({ type, placeholder, value, onChange, disabled }) {
+  return (
+    <input
+      value={value}
+      type={type}
+      onChange={onChange}
+      className="input"
+      placeholder={placeholder}
+      disabled={disabled}
+    />
+  );
 }
 
 export default Input;
