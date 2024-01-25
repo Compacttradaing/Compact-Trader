8b89228f9a7c8fca7b164c8f5ac50db7441c9300
Jamiu Abidemi Abdulkareem <106557387+jamiuabdulkareem@users.noreply.github.com>
2024-01-25 22:54:29 +0100

    Update Spinner.jsx

--- src/ui/Spinner.jsx
+++ src/ui/Spinner.jsx
@@ -3,7 +3,7 @@ import { LineWave } from "react-loader-spinner";
 function Spinner() {
   return (
     <div className="fixed top-0 left-0 h-screen w-full bg-slate-900/20 z-50 backdrop-blur-sm transition-all duration-300">
-      <div className="fixed top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] transition-all duration-300 px-5">
+      <div className="fixed top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] transition-all duration-300">
         <LineWave
           visible={true}
           height="200"
