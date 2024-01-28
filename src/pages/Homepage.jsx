da5d1f667739bc69d7783806252f7e25d0821db8
owoo <106557387+jamiuabdulkareem@users.noreply.github.com>
2024-01-28 18:49:46 +0100

    Update Homepage.jsx

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,6 +1,11 @@
 import HomepageNav from "../features/HomepageNav";
 import Button from "../ui/Button";
-import { IoCheckmarkCircle } from "react-icons/io5";
+import {
+  IoCalculatorOutline,
+  IoCheckmarkCircle,
+  IoFlashOutline,
+  IoTimeOutline,
+} from "react-icons/io5";
 
 function Homepage() {
   return (
@@ -15,7 +20,7 @@ function Homepage() {
           <h2 className="md:text-8xl text-3xl font-bold text-slate-900">
             Get Instant Payment.
           </h2>
-          <p className="text-center px-2 md:w-1/2 mt-4 mb-4 text-slate-500 font-medium">
+          <p className="text-center text-wrap px-2 md:w-1/2 mt-4 mb-4 text-slate-500 font-medium">
             Take pleasure in a flawless experience with our platform, where your
             peace of mind is ensure of mind is ensured by our commitment to safe
             and secure transactions.
@@ -40,10 +45,39 @@ function Homepage() {
         </div>
       </header>
       <main>
-        <section className="md:px-40">
-          <h1 className="text-3xl font-bold text-center text-slate-900 py-20">
+        <section className="md:px-40 py-20 grid place-items-center">
+          <h1 className="text-3xl font-bold text-center text-slate-900">
             Our Reputaion
           </h1>
+          <ul className="sm:flex gap-4 items-center justify-center sm:gap-10 sm:justify-between mt-16">
+            <li className="ring-1 ring-indigo-200 sm:mb-0 mb-5 rounded-sm drop-shadow-sm p-4 w-80">
+              <IoCalculatorOutline className="text-3xl text-indigo-900" />
+              <h2 className="font-bold text-lg text-slate-900 py-2">
+                Best Rates
+              </h2>
+              <p className="text-slate-500 text-sm">
+                Nullam senectus porttitor in eget. Eget rutrum leo interdum.
+              </p>
+            </li>
+            <li className="ring-1 ring-indigo-200 sm:mb-0 mb-5 rounded-sm drop-shadow-sm p-4 w-80">
+              <IoFlashOutline className="text-3xl text-indigo-900" />
+              <h2 className="font-bold text-lg text-slate-900 py-2">
+                Fast Trade
+              </h2>
+              <p className="text-slate-500 text-sm">
+                Nullam senectus porttitor in eget. Eget rutrum leo interdum.
+              </p>
+            </li>
+            <li className="ring-1 ring-indigo-200 rounded-sm drop-shadow-sm p-4 w-80">
+              <IoTimeOutline className="text-3xl text-indigo-900" />
+              <h2 className="font-bold text-lg text-slate-900 py-2">
+                Instant Payment
+              </h2>
+              <p className="text-slate-500 text-sm">
+                Nullam senectus porttitor in eget. Eget rutrum leo interdum.
+              </p>
+            </li>
+          </ul>
         </section>
       </main>
     </div>
